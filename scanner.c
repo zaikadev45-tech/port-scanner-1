652cae12afff5c82b49227e7d00081fdeee77148
zaikadev45-tech <252950044+zaikadev45-tech@users.noreply.github.com>
2026-05-30 19:41:19 -0300

    (PT-BR🇧🇷) [+] Usei o tempo limite para determinar se a porta está filtrada, [+] Coloquei a função "post" que pode ser usada no futuro para diferentes varreduras,

--- scanner.c
+++ scanner.c
@@ -3,31 +3,34 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/select.h>
+
+//TCP connect
 #include <unistd.h>
+#include <fcntl.h>
+
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 
+void post(const int status, const int port);
+
 int check_port(const char *ip, int port) {
-  int sock_fd;
   struct sockaddr_in target_addr;
 
   // 1. Create an active IPv4 TCP socket
-  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (sock_fd < 0) {
     perror("Socket creation failed");
     return -1;
   }
 
+  fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+
   // 2. Set a timeout so we don't hang forever on filtered ports
   struct timeval timeout;
   timeout.tv_sec = 2;       // 2 sec
   timeout.tv_usec = 0;
-
-  // Set timeout for the connection attempt
-  setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-  setsockopt(sock_fd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
 
   // 3.Configure target details
   target_addr.sin_family = AF_INET;
@@ -42,17 +45,30 @@ int check_port(const char *ip, int port) {
 
   // 4. Attempt to connect to the remote host
   // connect() return 0 if the three-way handshake succeds
-  int result = connect(sock_fd, (struct sockaddr *)&target_addr, sizeof(target_addr));
+  int status = connect(sock_fd, (struct sockaddr *)&target_addr, sizeof(target_addr));
+
+  fd_set fdset;
+  FD_ZERO(&fdset);
+  FD_SET(sock_fd, &fdset);
+  int result = select(sock_fd + 1, NULL, &fdset, NULL, &timeout);
+  
+  //status = 2:  FILTERED
+  //status = 1:  OPEN
+  //status = 0:  CLOSED
 
   // 5. Clean up the file descriptor immediately
-  close(sock_fd);
 
-  if (result == 0) {
-    return 1;   // Port is OPEN 
+  if (result > 0) {
+    int erro = 0;
+    socklen_t len = sizeof(erro);
+    getsockopt(sock_fd, SOL_SOCKET, SO_ERROR, &erro, &len);
+    status = (erro == 0) ? 1 : 0;  // 0 = CLOSED, 1 = OPEN
   }
-  else {
-    return 0;  // Port is CLOSED or FILTERED
-  }
+  else if(result == 0) {status = 2;}  // Port is CLOSED or FILTERED
+  else {status = 0;}
+
+  close(sock_fd);
+  return status;
 }
 
 int main(int argc, char *argv[] ) {
@@ -67,12 +83,21 @@ int main(int argc, char *argv[] ) {
 
     printf("[*] Probing %s on port %d...\n", target_ip, port);
 
-    if (check_port(target_ip, port)) {
-      printf("[+] Port %d is OPEN!\n", port);
-    }
-    else {
-      printf("[-] Port %d is CLOSED or FILTERED\n", port);
-    }
+    post(check_port(target_ip, port), port);
 
     return 0;
+}
+
+void post(const int status, const int port) {
+    switch (status) {
+        case 2:
+            printf("[-] Port %d is FILTERED\n", port);
+            break;
+        case 1:
+            printf("[+] Port %d is OPEN!\n", port);
+            break;
+        case 0:
+            printf("[-] Port %d is CLOSED\n", port);
+            break;
+    }
 }
